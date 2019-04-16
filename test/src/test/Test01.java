8400ca6025ac72385586bab5bf76abbc71742de7
SurfingBoy <surfingcode@163.com>
2019-04-16 13:27:39 +0800

    jvm

--- test/src/test/Test01.java
+++ test/src/test/Test01.java
@@ -10,6 +10,7 @@ public class Test01 {
 		
 		byte[] b1=new byte[1*1024*1024];
 		
+		
 		System.out.println("max memory:"+Runtime.getRuntime().maxMemory()/1024/1024);
 		System.out.println("free memory:"+Runtime.getRuntime().freeMemory()/1024/1024);
 		System.out.println("total memory:"+Runtime.getRuntime().totalMemory()/1024/1024);
